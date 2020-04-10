f1827564bc41ebbf5fe0ebdf4f0ceed7e27ed377
MatthewScott32 <matthewscottblagg@gmail.com>
2020-04-10 18:01:39 -0500

    finished tuples chapter

---
+++ zoo.py
@@ -0,0 +1,12 @@
+zoo = ("bear", "eagle", "wolf", "lion", "tiger", "snake", "cheetah", "T-Rex", "Shark", "Gorilla")
+print(zoo.index("T-Rex"))
+
+animal_to_find = "Gorilla"
+if animal_to_find in zoo:
+    print("animal found")
+
+(first_animal, second_animal, third_animal, fourth_animal, fifth_animal, sixth_animal, seventh_animal, eigth_animal, ninth_animal, tenth_animal) = zoo
+print(first_animal)
+print(second_animal)
+print(third_animal)
+print(fourth_animal)
